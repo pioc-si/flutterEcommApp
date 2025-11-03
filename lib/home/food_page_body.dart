cb8894aed5f30379ede7687723b32d0fc162d0aa
pioc_si <victoriia.davydova@yandex.ru>
2025-11-03 21:25:08 +0300

    add shadow under small food body widget

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -38,7 +38,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.redAccent,
+      //color: Colors.redAccent,
       height: 320,
       child: PageView.builder(
         controller: pageController,
@@ -102,8 +102,23 @@ class _FoodPageBodyState extends State<FoodPageBody> {
               height: 120,
               margin: EdgeInsets.only(left: 30, right: 30, bottom: 30),
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30),
+                borderRadius: BorderRadius.circular(20),
                 color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Color(0xFFe8e8e8),
+                    blurRadius: 5.0,
+                    offset: Offset(0, 5) 
+                  ),
+                  BoxShadow(
+                    color: Colors.white,
+                    offset: Offset(-5,0),
+                  ),
+                  BoxShadow(
+                    color: Colors.white,
+                    offset: Offset(5,0),
+                  ),
+                ]
               ),
               child: Container(
                 padding: EdgeInsets.only(top: 15, left: 15, right: 15 ),
@@ -153,6 +168,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
                   ],
                 )
               ),
+
             ),
           ),
         ],
