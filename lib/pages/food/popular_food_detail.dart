f128db22c9f7d7948fe2e948e238d3dc2e2283d4
pioc_si <victoriia.davydova@yandex.ru>
2025-11-06 14:19:43 +0300

    add gest det on pop food page and back to main page

--- lib/pages/food/popular_food_detail.dart
+++ lib/pages/food/popular_food_detail.dart
@@ -8,6 +8,9 @@ import 'package:ecommapp/widgets/icon_and_text_widget.dart';
 import 'package:ecommapp/widgets/small_text.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../home/main_food_page.dart';
 
 class PopularFoodDetail extends StatelessWidget {
   const PopularFoodDetail({super.key});
@@ -39,7 +42,12 @@ class PopularFoodDetail extends StatelessWidget {
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                AppIcon(icon: Icons.arrow_back_ios),
+                GestureDetector(
+                  onTap: (){
+                    Get.to(()=>MainFoodPage());
+                  },
+                  child: 
+                  AppIcon(icon: Icons.arrow_back_ios)),
                 AppIcon(icon: Icons.shopping_cart_outlined)
 
 
