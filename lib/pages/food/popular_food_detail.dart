f686994ed542f782076af1c35e92e405c2edf4cf
pioc_si <victoriia.davydova@yandex.ru>
2025-11-06 18:02:21 +0300

    fix hight forbottom page  popfood

--- lib/pages/food/popular_food_detail.dart
+++ lib/pages/food/popular_food_detail.dart
@@ -112,7 +112,7 @@ class PopularFoodDetail extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Container(
-            padding: EdgeInsets.only(top: Dimensions.height20, bottom: Dimensions.height20, left: Dimensions.width20, right: Dimensions.width20),
+            padding: EdgeInsets.only(top: Dimensions.height20, bottom: Dimensions.height15, left: Dimensions.width20, right: Dimensions.width20),
             
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(Dimensions.radius20),
@@ -129,7 +129,7 @@ class PopularFoodDetail extends StatelessWidget {
           ),  
           ),
           Container(
-            padding: EdgeInsets.only(top: Dimensions.height20, bottom: Dimensions.height20, left: Dimensions.width20, right: Dimensions.width20),
+            padding: EdgeInsets.only(top: Dimensions.height20, bottom: Dimensions.height15, left: Dimensions.width20, right: Dimensions.width20),
             child: BigText(text: "\$10 | Add to cart", color: Colors.white,),
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(Dimensions.radius20),
