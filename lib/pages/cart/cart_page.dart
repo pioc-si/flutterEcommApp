8c8a3bbcbaf1f2b15a578a2038c203efa6f58eef
pioc_si <victoriia.davydova@yandex.ru>
2025-11-09 20:06:47 +0300

    add in cart go to pop or recom food page

--- lib/pages/cart/cart_page.dart
+++ lib/pages/cart/cart_page.dart
@@ -1,4 +1,6 @@
 import 'package:ecommapp/controllers/cart_controller.dart';
+import 'package:ecommapp/controllers/popular_product_controller.dart';
+import 'package:ecommapp/controllers/recommended_product_controller.dart';
 import 'package:ecommapp/pages/home/main_food_page.dart';
 import 'package:ecommapp/routes/route_helper.dart';
 import 'package:ecommapp/utils/app_constants.dart';
@@ -74,23 +76,39 @@ class CartPage extends StatelessWidget {
                       width: double.maxFinite,
                       child: Row(
                         children: [
-                          Container(
-                            width: Dimensions.height20 * 5,
-                            height: Dimensions.height20 * 5,
-                            margin: EdgeInsets.only(
-                              bottom: Dimensions.height10,
-                            ),
-                            decoration: BoxDecoration(
-                              image: DecorationImage(
-                                fit: BoxFit.cover,
-                                image: NetworkImage(
-                                  AppConstants.BASE_URL+AppConstants.UPLOAD_URL+cartController.getItems[index].img!
-                                )
+                          GestureDetector(
+                            onTap: () {
+                              var popularIndex = 
+                              Get.find<PopularProductController>()
+                              .popularProductList.indexOf(_cartList[index].product!);
+                              
+                              if(popularIndex>=0){
+                                  Get.toNamed(RouteHelper.getPopularFood(popularIndex));
+                              }else{
+                                var recommendedIndex = 
+                              Get.find<RecommendedProductController>()
+                              .recommendedProductList.indexOf(_cartList[index].product!);
+                              Get.toNamed(RouteHelper.getRecommendedFood(recommendedIndex));
+                              }
+                            },
+                            child: Container(
+                              width: Dimensions.height20 * 5,
+                              height: Dimensions.height20 * 5,
+                              margin: EdgeInsets.only(
+                                bottom: Dimensions.height10,
                               ),
-                              borderRadius: BorderRadius.circular(
-                                Dimensions.radius20,
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  fit: BoxFit.cover,
+                                  image: NetworkImage(
+                                    AppConstants.BASE_URL+AppConstants.UPLOAD_URL+cartController.getItems[index].img!
+                                  )
+                                ),
+                                borderRadius: BorderRadius.circular(
+                                  Dimensions.radius20,
+                                ),
+                                color: Colors.white,
                               ),
-                              color: Colors.white,
                             ),
                           ),
                           SizedBox(width: Dimensions.width10),
