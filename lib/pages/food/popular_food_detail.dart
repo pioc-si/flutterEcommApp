97d99cbc4efdd951cd14536d6128e1becfed6ad9
pioc_si <victoriia.davydova@yandex.ru>
2025-11-09 18:40:41 +0300

    add to cart when cart >=1

--- lib/pages/food/popular_food_detail.dart
+++ lib/pages/food/popular_food_detail.dart
@@ -62,16 +62,16 @@ class PopularFoodDetail extends StatelessWidget {
                 ),
 
                 
-                GetBuilder<PopularProductController>(
-                  builder: (controller) {
+                GetBuilder<PopularProductController>(builder: (controller) {
                     return GestureDetector(
                       onTap: () {
+                        if(controller.totalItems >= 1)
                         Get.toNamed(RouteHelper.getCartPage());
                       },
                       child: Stack(
                         children: [
                           AppIcon(icon: Icons.shopping_cart_outlined),
-                          Get.find<PopularProductController>().totalItems >= 1
+                          controller.totalItems >= 1
                               ? Positioned(
                                   right: 0,
                                   top: 0,
