3632c91c5c9d0a7cc82df9e3d0e7e4bcfb576ff1
pioc_si <victoriia.davydova@yandex.ru>
2025-11-09 16:07:27 +0300

    add adding in cart in recomm food detail

--- lib/pages/food/popular_food_detail.dart
+++ lib/pages/food/popular_food_detail.dart
@@ -175,25 +175,27 @@ class PopularFoodDetail extends StatelessWidget {
                     ],
                   ),  
                 ),
-                Container(
-                  padding: EdgeInsets.only(
-                    top: Dimensions.height20, 
-                    bottom: Dimensions.height15, 
-                    left: Dimensions.width20, 
-                    right: Dimensions.width20
-                  ),
-                  child: GestureDetector(
-                    onTap: (){
-                      popularProduct.addItem(product);
-                    },
-                    child: BigText(
-                      text: "\$ ${product.price!} | Add to cart", 
-                      color: Colors.white,
+                GestureDetector(
+                  onTap: () {
+                    popularProduct.addItem(product);
+                  },
+                  child: Container(
+                    padding: EdgeInsets.only(
+                      top: Dimensions.height20, 
+                      bottom: Dimensions.height15, 
+                      left: Dimensions.width20, 
+                      right: Dimensions.width20
                     ),
-                  ),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(Dimensions.radius20),
-                    color: AppColors.mainColor,
+                    
+                      child: BigText(
+                        text: "\$ ${product.price!} | Add to cart", 
+                        color: Colors.white,
+                      
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(Dimensions.radius20),
+                      color: AppColors.mainColor,
+                    ),
                   ),
                 ),
               ],
