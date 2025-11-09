51ad1eed025dcabc63bb46050da2879ad2868f53
pioc_si <victoriia.davydova@yandex.ru>
2025-11-09 16:18:33 +0300

    add cart is  not a zero, when add 0 quantity

--- lib/controllers/popular_product_controller.dart
+++ lib/controllers/popular_product_controller.dart
@@ -26,10 +26,8 @@ class PopularProductController extends GetxController {
   Future<void> getPopularProductList() async {
     Response response = await popularProductRepo.getPopularProductList();
     if (response.statusCode == 200) {
-      //print("got prs");
       _popularProductList = [];
       _popularProductList.addAll(Product.fromJson(response.body).products);
-      //print(_popularProductList);
       _isLoaded = true;
       update();
     } else {}
@@ -38,7 +36,6 @@ class PopularProductController extends GetxController {
   void setQuantity(bool isIncrement) {
     if (isIncrement) {
       _quantity = checkQuantity(_quantity + 1);
-      print("item of items " + quantity.toString());
     } else {
       _quantity = checkQuantity(_quantity - 1);
     }
@@ -53,6 +50,10 @@ class PopularProductController extends GetxController {
         backgroundColor: AppColors.mainColor,
         colorText: Colors.white,
       );
+      if(_inCartItems>0){
+        _quantity = -_inCartItems;
+        return _quantity;
+      }
       return 0;
     } else if ((_inCartItems + quantity) > 20) {
       Get.snackbar(
@@ -73,11 +74,9 @@ class PopularProductController extends GetxController {
     _cart = cart;
     var exist = false;
     exist = _cart.existInCart(product);
-    print("existing: " + exist.toString());
     if (exist) {
       _inCartItems = _cart.getQuantity(product);
     }
-    print("quantity " + _inCartItems.toString());
   }
 
   void addItem(ProductModel product) {
